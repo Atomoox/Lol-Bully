52f3909db17b1408ee376d595b546de55da309db
Atomox_ <aoxresell@gmail.com>
2022-02-24 13:46:02 +0100

    Added startup example

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import { DiscordWrapper } from './discordWrappper';
 import { Events } from './types';
 
 class Application {
-    public static async run() {
-        const apiWrapper = new ApiWrapper('RGAPI-b9dfc610-b8b5-4668-a18f-cb3fda766522');
-        const discordWrapper = new DiscordWrapper("", "https://discord.com/api/webhooks/944165432657600562/JBd8D6qpT9T5wNlILdqiBG4wbd_xhUDPcHNTj9bQmsldMCiGv8B4iBKPswPpcq2kQv0X");
+    public static async run(API_KEY: string, userName: string, webhook: string) {
+        const apiWrapper = new ApiWrapper(API_KEY);
+        const discordWrapper = new DiscordWrapper(webhook);
 
         apiWrapper.on(Events.Loss, (matchData: any, parsedDatas: any) => {
             discordWrapper.submitWebhook(matchData);
@@ -18,8 +18,11 @@ class Application {
             console.log(parsedDatas);
          })
 
-        await apiWrapper.start("EMDDD")
+        await apiWrapper.start(userName);
     }
 };
-
-Application.run();
+/*
+(async () => {
+    Application.run("", "", "");
+})();
+*/ 
